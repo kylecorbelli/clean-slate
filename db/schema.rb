ad2ac186a87b1d6933f6520ba67da0fb0777c7c3
Kyle Corbelli <kyle.corbelli@gmail.com>
2017-04-01 15:14:17 -0700

    Add List and Task models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170401213413) do
+ActiveRecord::Schema.define(version: 20170401215454) do
+
+  create_table "lists", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_lists_on_user_id", using: :btree
+  end
+
+  create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "description"
+    t.boolean  "is_done",     default: false
+    t.integer  "list_id"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.index ["list_id"], name: "index_tasks_on_list_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "provider",                             default: "email", null: false
@@ -40,4 +57,6 @@ ActiveRecord::Schema.define(version: 20170401213413) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
   end
 
+  add_foreign_key "lists", "users"
+  add_foreign_key "tasks", "lists"
 end
