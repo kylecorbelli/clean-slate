0a196fec79efa4e2dff954b92a117771dda31ce4
Kyle Corbelli <kyle.corbelli@gmail.com>
2017-04-01 14:16:04 -0700

    Remove dummy "people" resource used for testing prod site
    Run down the migration to create the "people" resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170401204553) do
-
-  create_table "people", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name"
-    t.integer  "age"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
